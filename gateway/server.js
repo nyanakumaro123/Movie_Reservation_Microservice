9674770aeb71974284cf28ae6639dcdb6cd11216
Joeyson Salam <170385559+joeyssn1@users.noreply.github.com>
2026-06-11 11:55:28 +0700

    changd port 8080

--- gateway/server.js
+++ gateway/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 // 1. Proxy API requests to Laravel (Passes the FULL path: /api/payments/charge)
 app.use(createProxyMiddleware({
